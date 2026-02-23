75df3f527e1553b968d13d46d7faa96ab941f6e1
Ali Algazar <ilamohamed12@gmail.com>
2026-02-23 10:27:50 +0200

    feat: Add OnboardingModel class to manage onboarding screen data

---
+++ lib/features/onboarding/presentation/model/onboarding_model.dart
@@ -0,0 +1,11 @@
+class OnboardingModel {
+  final String image;
+  final String title;
+  final String description;
+
+  const OnboardingModel({
+    required this.description,
+    required this.image,
+    required this.title,
+  });
+}
