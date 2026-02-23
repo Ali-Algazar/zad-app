afa1b4ed1626b530a87a6bf9db2ee97260547946
Ali Algazar <ilamohamed12@gmail.com>
2026-02-23 10:36:15 +0200

    feat: Add CustomBoton widget for reusable button component

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:zad/core/utils/app_colors.dart';
+
+class CustomBoton extends StatelessWidget {
+  const CustomBoton({super.key, required this.onTap, required this.child});
+
+  final VoidCallback onTap;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        height: 50,
+        decoration: BoxDecoration(
+          color: AppColors.primary,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Center(child: child),
+      ),
+    );
+  }
+}
