5b63aacfe1030f39fddb868d75234c773350eff8
Ali Algazar <ilamohamed12@gmail.com>
2026-02-23 10:27:37 +0200

    feat: Refactor text styles in AppTextStyles for improved readability and consistency

--- lib/core/utils/app_text_styles.dart
+++ lib/core/utils/app_text_styles.dart
@@ -1,31 +1,41 @@
 import 'package:flutter/material.dart';
 
 class AppTextStyles {
-  static const TextStyle header = TextStyle(
+  static const TextStyle textStyle24 = TextStyle(
     fontSize: 24,
     fontWeight: FontWeight.w600,
     height: 1.3,
   );
+  static const TextStyle textStyle32 = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+    height: 1.4,
+  );
+  static const TextStyle textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 1.4,
+  );
 
-  static const TextStyle button = TextStyle(
+  static const TextStyle textStyle18 = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w600,
     height: 1.56,
   );
 
-  static const TextStyle bodyBold = TextStyle(
+  static const TextStyle textStyle14Bold = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.w700,
     height: 1.43,
   );
 
-  static const TextStyle bodyMedium = TextStyle(
+  static const TextStyle textStyle14Medium = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.w500,
     height: 1.43,
   );
 
-  static const TextStyle bodyRegular = TextStyle(
+  static const TextStyle textStyle14r = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.w400,
     height: 1.43,
