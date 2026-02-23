e0a42ee0ba7b064b22aa7eb6397b301303de954d
Ali Algazar <ilamohamed12@gmail.com>
2026-02-23 10:28:37 +0200

    feat: Add route for OnboardingView in onGenerateRoute function

--- lib/core/helper_functions/on_generate_routes.dart
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:zad/features/splash/presentation/view/splash_view.dart';
+import 'package:zad/features/onboarding/presentation/view/onboarding_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case SplashView.routeName:
       return MaterialPageRoute(builder: (_) => const SplashView());
+
+    case OnboardingView.routeName:
+      return MaterialPageRoute(builder: (_) => const OnboardingView());
 
     default:
       return MaterialPageRoute(
