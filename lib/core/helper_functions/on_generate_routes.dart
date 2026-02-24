34aa2458cc2a2d97efde97d6a4318c61366985d2
Ali Algazar <ilamohamed12@gmail.com>
2026-02-24 13:49:09 +0200

    feat: Implement CreateAccountViewBody with form handling and user input fields

--- lib/core/helper_functions/on_generate_routes.dart
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:zad/features/auth/presentation/view/create_account_view.dart';
+import 'package:zad/features/auth/presentation/view/forgot_password_view.dart';
 import 'package:zad/features/auth/presentation/view/login_view.dart';
+import 'package:zad/features/auth/presentation/view/reset_password_view.dart';
+import 'package:zad/features/auth/presentation/view/verify_code_view.dart';
 import 'package:zad/features/splash/presentation/view/splash_view.dart';
 import 'package:zad/features/onboarding/presentation/view/onboarding_view.dart';
 
@@ -17,6 +20,13 @@ Route<dynamic> onGenerateRoute(RouteSettings settings) {
 
     case CreateAccountView.routeName:
       return MaterialPageRoute(builder: (_) => const CreateAccountView());
+
+    case ForgotPasswordView.routeName:
+      return MaterialPageRoute(builder: (_) => const ForgotPasswordView());
+    case ResetPasswordView.routeName:
+      return MaterialPageRoute(builder: (_) => const ResetPasswordView());
+    case VerifyCodeView.routeName:
+      return MaterialPageRoute(builder: (_) => const VerifyCodeView());
 
     default:
       return MaterialPageRoute(
