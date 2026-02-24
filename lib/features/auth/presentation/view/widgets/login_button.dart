1ada5c6f6ed6d3f960f0c3d75937e3e506d664fe
Ali Algazar <ilamohamed12@gmail.com>
2026-02-24 13:47:57 +0200

    feat: Add LoginButton widget for user authentication

---
+++ lib/features/auth/presentation/view/widgets/login_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:zad/core/utils/app_colors.dart';
+import 'package:zad/core/utils/app_text_styles.dart';
+import 'package:zad/core/widgets/custom_button.dart';
+import 'package:zad/generated/l10n.dart';
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({super.key, required GlobalKey<FormState> formKey})
+    : _formKey = formKey;
+
+  final GlobalKey<FormState> _formKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomButton(
+      onTap: () {
+        if (_formKey.currentState!.validate()) {}
+      },
+      child: Text(
+        S.of(context).login,
+        style: AppTextStyles.textStyle16.copyWith(color: AppColors.background),
+      ),
+    );
+  }
+}
