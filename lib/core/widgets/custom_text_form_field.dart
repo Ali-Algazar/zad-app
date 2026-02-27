9870a932ebe7d6427b3386dbe9fabc5bd6e7191d
Ali Algazar <ilamohamed12@gmail.com>
2026-02-27 13:30:43 +0200

    feat: Enhance CustomTextFormField to support maxLength, onChanged, and errorText properties

--- lib/core/widgets/custom_text_form_field.dart
+++ lib/core/widgets/custom_text_form_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:zad/core/extensions/extensions.dart';
 import 'package:zad/core/utils/app_colors.dart';
 import 'package:zad/core/utils/app_text_styles.dart';
@@ -13,6 +14,9 @@ class CustomTextFormField extends StatelessWidget {
     this.validator,
     this.keyboardType,
     this.obscureText = false,
+    this.onChanged,
+    this.maxLength,
+    this.errorText,
   });
 
   final TextEditingController? controller;
@@ -22,16 +26,22 @@ class CustomTextFormField extends StatelessWidget {
   final String? Function(String?)? validator;
   final TextInputType? keyboardType;
   final bool obscureText;
+  final void Function(String)? onChanged;
+  final int? maxLength;
+  final String? errorText;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      maxLength: maxLength,
+      onChanged: onChanged,
       obscureText: obscureText,
       controller: controller,
       validator: validator,
       keyboardType: keyboardType,
       decoration: InputDecoration(
         contentPadding: 8.all,
+        errorText: errorText,
 
         hintText: hintText,
         hintStyle: AppTextStyles.caption.copyWith(
