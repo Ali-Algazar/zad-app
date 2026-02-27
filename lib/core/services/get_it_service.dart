433a0db10c60530e9cba37c7b8c768bbb027efbe
Ali Algazar <ilamohamed12@gmail.com>
2026-02-27 13:32:30 +0200

    feat: Implement service locator setup for dependency injection of Dio, ApiHelper, AuthLocalDataSource, AuthRemoteDataSource, and AuthRepository

--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -1,5 +1,38 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:zad/core/services/api_helper.dart';
+import 'package:zad/features/auth/data/datasources/auth_local_data_source.dart';
+import 'package:zad/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:zad/features/auth/data/repositories/auth_repository.dart';
+import 'package:zad/features/auth/data/repositories/auth_repository_impl.dart';
 
 final sl = GetIt.instance;
 
-void setupServiceLocator() {}
+void setupServiceLocator() {
+  sl.registerLazySingleton<Dio>(
+    () => Dio(
+      BaseOptions(
+        receiveDataWhenStatusError: true,
+        validateStatus: (status) {
+          return status! < 500;
+        },
+      ),
+    ),
+  );
+  sl.registerLazySingleton<ApiHelper>(() => ApiHelper(sl<Dio>()));
+
+  sl.registerLazySingleton<AuthLocalDataSource>(
+    () => AuthLocalDataSourceImpl(),
+  );
+
+  sl.registerLazySingleton<AuthRemoteDataSource>(
+    () => AuthRemoteDataSourceImpl(sl<ApiHelper>()),
+  );
+
+  sl.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(
+      localDataSource: sl<AuthLocalDataSource>(),
+      remoteDataSource: sl<AuthRemoteDataSource>(),
+    ),
+  );
+}
