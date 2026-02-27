d93facf0b79f79c613efaab085d5768b67d59dc6
Ali Algazar <ilamohamed12@gmail.com>
2026-02-27 13:30:26 +0200

    feat: Enhance CustomButton to include isActive property for conditional tap handling and styling

--- lib/core/widgets/custom_button.dart
+++ lib/core/widgets/custom_button.dart
@@ -2,20 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:zad/core/utils/app_colors.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.onTap, required this.child});
+  const CustomButton({
+    super.key,
+    required this.onTap,
+    required this.child,
+    this.isActtive = true,
+  });
 
   final VoidCallback onTap;
   final Widget child;
+  final bool isActtive;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: onTap,
+      onTap: isActtive ? onTap : null,
       child: Container(
         width: double.infinity,
         height: 50,
         decoration: BoxDecoration(
-          color: AppColors.primary,
+          color: isActtive
+              ? AppColors.primary
+              : AppColors.primary.withValues(alpha: 0.5),
           borderRadius: BorderRadius.circular(12),
         ),
         child: Center(child: child),
