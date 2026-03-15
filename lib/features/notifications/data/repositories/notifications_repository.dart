18d4c1ac1dcf1b9f3e0e9d2edeae53d8df8b89c6
Ali Algazar <ilamohamed12@gmail.com>
2026-03-15 09:54:52 +0200

    feat: implement NotificationsRepository with methods for fetching and marking notifications

--- lib/features/notifications/data/repositories/notifications_repository.dart
+++ lib/features/notifications/data/repositories/notifications_repository.dart
@@ -1,4 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:zad/core/errors/failures.dart';
+import 'package:zad/features/notifications/data/model/notification_model.dart';
 
 abstract class NotificationsRepository {
-  // Future<void> getNotifications(String id);
+  Future<Either<Failure, List<NotificationModel>>> getNotifications();
+  Future<Either<Failure, Unit>> markAllAsRead();
+  Future<Either<Failure, Unit>> markAsRead(String notificationId);
 }
--- lib/features/notifications/data/repositories/notifications_repository_impl.dart
+++ lib/features/notifications/data/repositories/notifications_repository_impl.dart
@@ -1,20 +1,43 @@
-
+import 'package:dartz/dartz.dart';
+import 'package:zad/core/errors/failures.dart';
+import 'package:zad/features/notifications/data/datasources/notifications_remote_data_source.dart';
+import 'package:zad/features/notifications/data/model/notification_model.dart';
 import 'notifications_repository.dart';
-// (اختياري) يمكنك استيراد الـ datasources
-// import '../datasources/notifications_remote_data_source.dart';
-// import '../datasources/notifications_local_data_source.dart';
 
 class NotificationsRepositoryImpl implements NotificationsRepository {
-  // final NotificationsRemoteDataSource remoteDataSource;
-  // final NotificationsLocalDataSource localDataSource;
+  final NotificationsRemoteDataSource remoteDataSource;
+  NotificationsRepositoryImpl({required this.remoteDataSource});
 
-  // NotificationsRepositoryImpl({
-  //   required this.remoteDataSource,
-  //   required this.localDataSource,
-  // });
+  @override
+  Future<Either<Failure, List<NotificationModel>>> getNotifications() async {
+    try {
+      final response = await remoteDataSource.getNotifications();
+      final notifications = (response.data['notifications'] as List)
+          .map((json) => NotificationModel.fromJson(json))
+          .toList();
+      return Right(notifications);
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
 
-  // @override
-  // Future<void> getNotifications(String id) async {
-  //   // ...
-  // }
+  @override
+  Future<Either<Failure, Unit>> markAllAsRead() async {
+    try {
+      await remoteDataSource.markAllAsRead();
+      return Right(unit);
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Unit>> markAsRead(String notificationId) async {
+    try {
+      await remoteDataSource.markAsRead(notificationId);
+      return Right(unit);
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
 }
