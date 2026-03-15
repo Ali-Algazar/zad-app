295dd74c6821dd799c97d6a26547d42369365b52
Ali Algazar <ilamohamed12@gmail.com>
2026-03-15 10:04:10 +0200

    feat: enhance NotificationsCubit and NotificationsState with methods for marking notifications as read

--- lib/features/notifications/presentation/cubit/notifications_cubit.dart
+++ lib/features/notifications/presentation/cubit/notifications_cubit.dart
@@ -1,6 +1,37 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:zad/features/notifications/data/repositories/notifications_repository.dart';
 import 'notifications_state.dart';
 
 class NotificationsCubit extends Cubit<NotificationsState> {
-  NotificationsCubit() : super(NotificationsInitial());
+  NotificationsCubit(this.notificationsRepository)
+    : super(NotificationsInitial());
+  final NotificationsRepository notificationsRepository;
+  bool isAllRead = false;
+
+  Future<void> getNotifications() async {
+    emit(NotificationsLoading());
+    final result = await notificationsRepository.getNotifications();
+    result.fold((failure) => emit(NotificationsError(failure.message)), (
+      notifications,
+    ) {
+      isAllRead = notifications.every((notification) => notification.isRead);
+      emit(NotificationsLoaded(notifications));
+    });
+  }
+
+  Future<void> markAllAsRead() async {
+    final response = await notificationsRepository.markAllAsRead();
+    response.fold(
+      (failure) => emit(NotificationsError(failure.message)),
+      (_) => getNotifications(),
+    );
+  }
+
+  Future<void> markAsRead(String notificationId) async {
+    final response = await notificationsRepository.markAsRead(notificationId);
+    response.fold(
+      (failure) => emit(NotificationsError(failure.message)),
+      (_) => getNotifications(),
+    );
+  }
 }
--- lib/features/notifications/presentation/cubit/notifications_state.dart
+++ lib/features/notifications/presentation/cubit/notifications_state.dart
@@ -1,3 +1,21 @@
+import 'package:zad/features/notifications/data/model/notification_model.dart';
+
 abstract class NotificationsState {}
 
 class NotificationsInitial extends NotificationsState {}
+
+class NotificationsLoading extends NotificationsState {}
+
+class NotificationsLoaded extends NotificationsState {
+  final List<NotificationModel> notifications;
+  NotificationsLoaded(this.notifications);
+}
+
+class NotificationsError extends NotificationsState {
+  final String message;
+  NotificationsError(this.message);
+}
+
+class MarkingAllAsRead extends NotificationsState {}
+
+class MarkingAsRead extends NotificationsState {}
