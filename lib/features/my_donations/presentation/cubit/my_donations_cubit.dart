f1bed8e4358bc4e0c073d9216b108a4249d12d9a
Ali Algazar <ilamohamed12@gmail.com>
2026-03-15 08:41:31 +0200

    feat: implement MyDonationsCubit and MyDonationsState with loading, error, and loaded states

--- lib/features/my_donations/presentation/cubit/my_donations_cubit.dart
+++ lib/features/my_donations/presentation/cubit/my_donations_cubit.dart
@@ -1,6 +1,17 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:zad/features/my_donations/data/repositories/my_donations_repository.dart';
 import 'my_donations_state.dart';
 
 class MyDonationsCubit extends Cubit<MyDonationsState> {
-  MyDonationsCubit() : super(MyDonationsInitial());
+  MyDonationsCubit(this.repository) : super(MyDonationsInitial());
+  final MyDonationsRepository repository;
+
+  Future<void> getMyDonations() async {
+    emit(MyDonationsLoading());
+    final result = await repository.getMyDonations();
+    result.fold(
+      (failure) => emit(MyDonationsError(message: failure.message)),
+      (donations) => emit(MyDonationsLoaded(donations: donations)),
+    );
+  }
 }
--- lib/features/my_donations/presentation/cubit/my_donations_state.dart
+++ lib/features/my_donations/presentation/cubit/my_donations_state.dart
@@ -1,3 +1,17 @@
+import 'package:zad/features/my_donations/data/model/donation_model.dart';
+
 abstract class MyDonationsState {}
 
 class MyDonationsInitial extends MyDonationsState {}
+
+class MyDonationsLoaded extends MyDonationsState {
+  List<DonationModel> donations;
+  MyDonationsLoaded({required this.donations});
+}
+
+class MyDonationsError extends MyDonationsState {
+  String message;
+  MyDonationsError({required this.message});
+}
+
+class MyDonationsLoading extends MyDonationsState {}
