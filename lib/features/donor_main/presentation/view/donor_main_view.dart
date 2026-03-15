f3849e03cbf0871b25f595000530dc2a30dab8e0
Ali Algazar <ilamohamed12@gmail.com>
2026-03-15 11:02:55 +0200

    feat: integrate NotificationsCubit into DonorMainView and update NotificationsButon to reflect notification state

--- lib/features/donor_main/presentation/view/donor_main_view.dart
+++ lib/features/donor_main/presentation/view/donor_main_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:zad/core/services/get_it_service.dart';
+import 'package:zad/features/notifications/data/repositories/notifications_repository.dart';
+import 'package:zad/features/notifications/presentation/cubit/notifications_cubit.dart';
 import 'widgets/donor_main_view_body.dart';
 
 class DonorMainView extends StatelessWidget {
@@ -7,6 +11,10 @@ class DonorMainView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: SafeArea(child: DonorMainViewBody()));
+    return BlocProvider(
+      create: (context) =>
+          NotificationsCubit(sl<NotificationsRepository>())..getNotifications(),
+      child: const Scaffold(body: SafeArea(child: DonorMainViewBody())),
+    );
   }
 }
--- lib/features/home_donor/presentation/view/widgets/header/notifications_button.dart
+++ lib/features/home_donor/presentation/view/widgets/header/notifications_button.dart
@@ -2,41 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:zad/core/cubit/nav_cubit.dart';
 import 'package:zad/core/utils/app_colors.dart';
+import 'package:zad/features/notifications/presentation/cubit/notifications_cubit.dart';
+import 'package:zad/features/notifications/presentation/cubit/notifications_state.dart';
 
 class NotificationsButon extends StatelessWidget {
   const NotificationsButon({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        context.read<NavCubit>().changeNavIndex(2);
-      },
-      child: CircleAvatar(
-        radius: 22,
-        backgroundColor: AppColors.grayText.withValues(alpha: 0.2),
-        child: Stack(
-          children: [
-            const Icon(
-              Icons.notifications_none_outlined,
-              color: Colors.white,
-              size: 24,
-            ),
-            Positioned(
-              right: 0,
-              top: 0,
-              child: Container(
-                width: 8,
-                height: 8,
-                decoration: BoxDecoration(
-                  color: Colors.redAccent,
-                  shape: BoxShape.circle,
+    return BlocBuilder<NotificationsCubit, NotificationsState>(
+      builder: (context, state) {
+        return InkWell(
+          onTap: () {
+            context.read<NavCubit>().changeNavIndex(2);
+          },
+          child: CircleAvatar(
+            radius: 22,
+            backgroundColor: AppColors.grayText.withValues(alpha: 0.2),
+            child: Stack(
+              children: [
+                const Icon(
+                  Icons.notifications_none_outlined,
+                  color: Colors.white,
+                  size: 24,
                 ),
-              ),
+                Positioned(
+                  right: 0,
+                  top: 0,
+                  child: Container(
+                    width: 8,
+                    height: 8,
+                    decoration: BoxDecoration(
+                      color: context.read<NotificationsCubit>().isAllRead
+                          ? Colors.redAccent
+                          : Colors.transparent,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
