e1e96f501da804cb0c3243a7097d6627d7dcd62f
Ali Algazar <ilamohamed12@gmail.com>
2026-03-16 08:07:39 +0200

    feat: implement getNearbyDonations method in HomeVolunteerRepositoryImpl for fetching nearby donations

--- lib/features/home_volunteer/data/repositories/home_volunteer_repository.dart
+++ lib/features/home_volunteer/data/repositories/home_volunteer_repository.dart
@@ -1,4 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:zad/core/errors/failures.dart';
+import 'package:zad/features/home_volunteer/data/model/donation_with_restaurant_model.dart';
 
 abstract class HomeVolunteerRepository {
-  // Future<void> getHomeVolunteer(String id);
+  Future<Either<Failure, List<DonationWithRestaurantModel>>>
+  getNearbyDonations();
 }
--- lib/features/home_volunteer/data/repositories/home_volunteer_repository_impl.dart
+++ lib/features/home_volunteer/data/repositories/home_volunteer_repository_impl.dart
@@ -1,20 +1,54 @@
+import 'dart:math';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:dartz/dartz.dart';
+import 'package:zad/core/errors/failures.dart';
+import 'package:zad/core/helper_functions/has_connection.dart';
+import 'package:zad/features/home_volunteer/data/datasources/home_volunteer_remote_data_source.dart';
+import 'package:zad/features/home_volunteer/data/model/donation_with_restaurant_model.dart';
 
 import 'home_volunteer_repository.dart';
-// (اختياري) يمكنك استيراد الـ datasources
-// import '../datasources/home_volunteer_remote_data_source.dart';
-// import '../datasources/home_volunteer_local_data_source.dart';
 
 class HomeVolunteerRepositoryImpl implements HomeVolunteerRepository {
-  // final HomeVolunteerRemoteDataSource remoteDataSource;
-  // final HomeVolunteerLocalDataSource localDataSource;
+  final HomeVolunteerRemoteDataSource remoteDataSource;
+  final Connectivity connectivity;
 
-  // HomeVolunteerRepositoryImpl({
-  //   required this.remoteDataSource,
-  //   required this.localDataSource,
-  // });
+  HomeVolunteerRepositoryImpl({
+    required this.remoteDataSource,
+    required this.connectivity,
+  });
 
-  // @override
-  // Future<void> getHomeVolunteer(String id) async {
-  //   // ...
-  // }
+  @override
+  Future<Either<Failure, List<DonationWithRestaurantModel>>>
+  getNearbyDonations() async {
+    try {
+      if (await hasConnection(connectivity)) {
+        final donations = await remoteDataSource.getNearbyDonations();
+        if (donations.statusCode == 200) {
+          final List<DonationWithRestaurantModel> donationList =
+              (donations.data['data'] as List)
+                  .map(
+                    (donation) =>
+                        DonationWithRestaurantModel.fromJson(donation),
+                  )
+                  .toList();
+          return Right(donationList);
+        } else {
+          return Left(
+            ServerFailure(
+              'Failed to fetch nearby donations with status code: ${donations.statusCode}',
+            ),
+          );
+        }
+      } else {
+        return Left(
+          NetworkFailure(
+            'No internet connection. Please check your connectivity.',
+          ),
+        );
+      }
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
 }
